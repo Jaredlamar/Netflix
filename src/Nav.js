6a94f92dc95792616014a71a8619cc4528ae1d68
Jared <redlaward@gmail.com>
2022-06-14 12:27:19 -0400

    New Project

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className='Nav'>
+        <img className='nav_logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png?20190206123158' />
+
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/requests.js
@@ -0,0 +1,16 @@
+import App from "./App";
+
+const API_KEY = "da1feb31bb9944887407b382915a3946";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_keys=${API_KEY}&with_networks=213`,
+    fetchToprated: `/movie/top_rated?api_keys=${API_KEY}&langauage=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorroMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
